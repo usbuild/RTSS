f73c24436bbf655c771e1b43b0959f42a910499b
usbuild <njuzhangqihcao@gmail.com>
2013-02-07 17:29:14 +0800

    add client main frame

--- client.c
+++ client.c
@@ -2,28 +2,44 @@
 #include <gtk/gtk.h>
 
 #define W_G(builder,type,name) name=GTK_##type(gtk_builder_get_object(builder,#name))
-#define NCALLBACK_(event_name) void event_name(GtkWidget *widget, gpointer user_data)
+#define N_CALLBACK(event_name) void event_name(GtkWidget *widget, gpointer user_data)
 
 
 GtkWindow *login_win;
 GtkButton *login_btn;
 GtkButton *cancel_btn;
-GtkDialog *aboutdialog1;
+GtkWindow *main_win;
 
 void get_widgets(GtkBuilder* gb)
 {
     W_G(gb,WINDOW,login_win);
     W_G(gb,BUTTON,login_btn);
     W_G(gb,BUTTON,cancel_btn);
-    W_G(gb,DIALOG,aboutdialog1);
+    W_G(gb,WINDOW,main_win);
 }
 
-NCALLBACK_(on_login_btn_clicked){
-    gtk_widget_show(GTK_WIDGET(aboutdialog1));
+N_CALLBACK(on_login_btn_clicked){
+    gtk_widget_hide(GTK_WIDGET(login_win));
+    gtk_widget_show(GTK_WIDGET(main_win));
 }
 
-NCALLBACK_(on_login_win_destroy) {
+N_CALLBACK(on_login_win_destroy) {
     gtk_main_quit();
+}
+
+N_CALLBACK(on_main_win_destroy) {
+    gtk_main_quit();
+}
+gboolean on_main_win_delete_event(GtkWindow *window, gpointer user_data) {
+    GtkMessageDialog* dialog = GTK_MESSAGE_DIALOG(gtk_message_dialog_new(
+        main_win,GTK_DIALOG_MODAL,GTK_MESSAGE_QUESTION,GTK_BUTTONS_YES_NO,"您真的要退出么?"
+        ));
+    gint result = gtk_dialog_run(GTK_DIALOG(dialog));
+    gtk_widget_destroy(GTK_WIDGET(dialog));
+    if(result == GTK_RESPONSE_YES) {
+        return FALSE;
+    }
+    return TRUE;
 }
 
 GtkBuilder* gtk_load_glade(gchar* filename) {
@@ -47,7 +63,8 @@ int main(int argc, char *argv[])
     get_widgets(gb);
 
 
-    gtk_widget_show(GTK_WIDGET(login_win));
+    //gtk_widget_show(GTK_WIDGET(login_win));
+    gtk_widget_show(GTK_WIDGET(main_win));
 
     gtk_main();
     return 0;
